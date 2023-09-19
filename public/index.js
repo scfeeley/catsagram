d8f07b1106ef6a1e144731b0f1afa3c4b9b5a757
Shannon Feeley <scfeeley@wpi.edu>
2023-09-19 11:33:00 -0600

    connected to cat api

--- public/index.js
+++ public/index.js
@@ -1,1 +1,22 @@
-// Your code here
+window.onload = (event => {
+    let body = document.body; 
+    let title = document.createElement("h1");
+    title.setAttribute("id", "title");
+    title.innerText = "Cat Pic";
+    body.appendChild(title);
+
+    let div = document.createElement("div");
+    div.setAttribute("class", "cat-box");
+    let img = document.createElement("img");
+    let catApi = "https://api.thecatapi.com/v1/images/search"
+    let catImgUrl;
+    const catImg = fetch(catApi)
+                    .then((res) => res.json())
+                    .then((data) => {
+                        img.setAttribute("src", data[0]["url"]);
+                        img.setAttribute("class", "cat-pic");
+                        img.setAttribute("id", "c1");
+                        div.appendChild(img);
+                        body.appendChild(div);
+                    })
+});
